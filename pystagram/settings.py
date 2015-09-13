430a4e2af2db14deb6d3ce508983d456b9d686dd
plusbeauxjours <plusbeauxjours@gmail.com>
2015-09-13 18:34:58 +0900

    넌 또
    왜...

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -111,13 +111,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'pystagram', 'static'),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 
 try:
     from django.contrib.messages import constants as messages
